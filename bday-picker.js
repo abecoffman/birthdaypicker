e2e2e0d4edddee3a0a24889fd46659b4c4c2c37e
Abrahm Coffman <abecoffman@gmail.com>
2011-03-21 22:32:27 -0700

    added two new options: defaultDate, hiddenDate

--- bday-picker.js
+++ bday-picker.js
@@ -12,49 +12,67 @@
   // plugin variables
   var months = {
     "short": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    "long": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  };
+    "long": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] },
+      curDate = new Date(),
+      curYear = curDate.getFullYear(),
+      curMonth = curDate.getMonth() + 1,
+      curDay = curDate.getDate();
+      
   
   $.fn.birthdaypicker = function( options ) {
     
     var settings = {
-      "maxAge"            : 120,
-      "futureDates"       : false,
-      "maxYear"           : curYear,
-      "dateFormat"        : "middleEndian",
-      "monthFormat"       : "short",
-      "placeholder"       : true,
-      "legend"            : ""
+      "maxAge"        : 120,
+      "futureDates"   : false,
+      "maxYear"       : curYear,
+      "dateFormat"    : "middleEndian",
+      "monthFormat"   : "short",
+      "placeholder"   : true,
+      "legend"        : "",
+      "defaultDate"   : false,
+      "hiddenDate"    : true
     };
-    
-    var curDate = new Date();
-    var curYear = curDate.getFullYear();
-    var curMonth = curDate.getMonth() + 1;
-    var curDay = curDate.getDate();
     
     return this.each(function() {
 
       if (options) { $.extend(settings, options); }
       
       // Create the html picker skeleton
-      var $fieldset = $("<fieldset class='birthday-picker'></fieldset>");
-      var $year = $("<select class='birth-year' name='birth[year]'></select>");
-      var $month = $("<select class='birth-month' name='birth[month]'></select>");
-      var $day = $("<select class='birth-day' name='birth[day]'></select>");
+      var $fieldset = $("<fieldset class='birthday-picker'></fieldset>"),
+          $year = $("<select class='birth-year' name='birth[year]'></select>"),
+          $month = $("<select class='birth-month' name='birth[month]'></select>"),
+          $day = $("<select class='birth-day' name='birth[day]'></select>");
       
       if(settings["legend"]) { $("<legend>" + settings["legend"] + "</legend>").appendTo($fieldset); }
+      
+      // Deal with the various Date Formats
+      var hiddenDate;
+      if(settings["dateFormat"] == "bigEndian") {
+        hiddenDate = curYear + "-" + curMonth + "-" + curDay;
+        $fieldset.append($year).append($month).append($day);
+      } else if(settings["dateFormat"] == "littleEndian") {
+        hiddenDate = curDay + "-" + curMonth + "-" + curYear;
+        $fieldset.append($day).append($month).append($year);
+      } else {
+        $fieldset.append($month).append($day).append($year);
+        hiddenDate = curMonth + "-" + curDay + "-" + curYear;
+      }
       
       // Add the option placeholders if specified
       if(settings["placeholder"]) {
         $("<option value='0'>Year:</option>").appendTo($year);
         $("<option value='0'>Month:</option>").appendTo($month);
         $("<option value='0'>Day:</option>").appendTo($day);
+        hiddenDate = null;
       }
       
-      // Deal with the various Date Formats
-      if(settings["dateFormat"] == "bigEndian") { $fieldset.append($year).append($month).append($day); }
-      else if(settings["dateFormat"] == "littleEndian") { $fieldset.append($day).append($month).append($year); }
-      else { $fieldset.append($month).append($day).append($year); }
+      // Account for a default date and set hiddenDate accordingly
+      if(settings["defaultDate"]) { hiddenDate = settings["defaultDate"] }
+      
+      // Create the hidden date markup
+      if(settings["hiddenDate"]) {
+        $("<input type='hidden' name='birthdate' />").val(hiddenDate).appendTo($fieldset);
+      }
       
       // Build the initial option sets
       var startYear = curYear;
@@ -66,15 +84,22 @@
       while(startYear >= endYear) { $("<option></option>").attr("value", startYear).text(startYear).appendTo($year); startYear--; }
       for( var j=0; j<12; j++) { $("<option></option>").attr("value", j+1).text(months[settings["monthFormat"]][j]).appendTo($month); }
       for(var k=1; k<32; k++) { $("<option></option>").attr("value", k).text(k).appendTo($day); }
-      var $this = $(this);
-      $this.append($fieldset);
+      $(this).append($fieldset);
+      
+      // Set the default date if given
+      if(settings["defaultDate"]) {
+        var date = new Date(settings["defaultDate"]);
+        $year.val(date.getFullYear());
+        $month.val(date.getMonth() + 1);
+        $day.val(date.getDate());
+      }
       
       // Update the option sets according to options and user selections
       $fieldset.change(function(){
-        var curNumMonths = parseInt($month.children(":last").val());
-        var curNumDays = parseInt($day.children(":last").val());
-        var dd = new Date($year.val(), $month.val(), 0);
-        var actNumDays = dd.getDate();
+        var curNumMonths = parseInt($month.children(":last").val()),
+            curNumDays = parseInt($day.children(":last").val()),
+            dd = new Date($year.val(), $month.val(), 0),
+            actNumDays = dd.getDate();
         // Dealing with the number of days in a month
         if(curNumDays > actNumDays) { while(curNumDays > actNumDays) { $day.children(":last").remove(); curNumDays--; } }
         else if(curNumDays < actNumDays) { 
@@ -99,6 +124,20 @@
           }
         }
         
+        // update the hidden date
+        var curYear = $year.val(),
+            curMonth = $month.val(),
+            curDay = $day.val();
+        if((curYear * curMonth * curDay) != 0) {
+          if(settings["dateFormat"] == "bigEndian") {
+            hiddenDate = curYear + "-" + curMonth + "-" + curDay;
+          } else if(settings["dateFormat"] == "littleEndian") {
+            hiddenDate = curDay + "-" + curMonth + "-" + curYear;
+          } else {
+            hiddenDate = curMonth + "-" + curDay + "-" + curYear;
+          }
+          $(this).children('input[type="hidden"]').val(hiddenDate);
+        }
       });
     });
   };
